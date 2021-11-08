30c6c1c2d759284edfa2276f8d47c92f8b0c109c
krishan <krishansorout131@gmail.com>
2021-11-08 12:09:41 +0530

    Updates

---
+++ precache-manifest.e38b54c6eccabfcf502837d1276ac3b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d81e245a3eeba449638963778a0b133",
+    "url": "/jobs-list/index.html"
+  },
+  {
+    "revision": "e3dc37ae85111b460d12",
+    "url": "/jobs-list/static/js/2.071da586.chunk.js"
+  },
+  {
+    "revision": "e1569e2c14f505dcc972",
+    "url": "/jobs-list/static/js/main.b625e102.chunk.js"
+  },
+  {
+    "revision": "7d483e2133c144f02931",
+    "url": "/jobs-list/static/js/runtime~main.8ef4291b.js"
+  }
+]);
